b0dfe2166102e4553737f27bddee025115d0006e
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2021-06-14 12:49:19 +0300

    slide plugin

---
+++ app.js
@@ -0,0 +1,20 @@
+function slidePlugin(activeSlide) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+
+  slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  });
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+
+slidePlugin(3);
